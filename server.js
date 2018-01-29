6981bcf21217b3f6d2319d2d541c0b8ab8872257
Acharya Raja Sekhar Brundhavanam <acharya.r@live.com>
2018-01-29 13:25:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', function (req, res, next) {
-    res.sendFile(path.join(__dirname + "\\home.html"));
+    res.sendFile(path.join(__dirname + "/home.html"));
 });
 
 app.post('/requested', leavesCtrl.validate, leavesCtrl.requested, mailDispatcher);
