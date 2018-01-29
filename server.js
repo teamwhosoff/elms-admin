afdaa4bce8628286491a7e6fa27c0a3924361032
Acharya Raja Sekhar Brundhavanam <acharya.r@live.com>
2018-01-29 13:23:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,6 +22,6 @@ app.post('/approved', leavesCtrl.validate, leavesCtrl.approved, mailDispatcher);
 app.post('/declined', leavesCtrl.validate, leavesCtrl.declined, mailDispatcher);
 app.post('/cancelled', leavesCtrl.validate, leavesCtrl.canceled, mailDispatcher);
 
-let server = app.listen(process.env.PORT | 8081, () => {
+let server = app.listen(process.env.PORT, () => {
     console.log("App listening at http://%s:%s", server.address().address, server.address().port);
 }); // taskkill /f /im node.exe
