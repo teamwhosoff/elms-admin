d9599179ff7c882d2051061405e2c670e4fc6c02
Acharya Raja Sekhar Brundhavanam <acharya.r@live.com>
2018-01-31 09:13:33 +0000

    fb admin init app

--- middleware/firebase-token-validator.js
+++ middleware/firebase-token-validator.js
@@ -1,5 +1,7 @@
 var admin = require('firebase-admin');
-admin.initializeApp();
+admin.initializeApp({
+  credential: admin.credential.cert(process.env.FIREBASE_CONFIG)
+});
 
 module.exports = (req, res, next) => {
     var idToken = req.headers['fb-user-token'];
