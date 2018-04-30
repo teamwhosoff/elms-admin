52f42aca572a7771bb82fa63fc86368edeaadc49
teamwhosoff <38849741+teamwhosoff@users.noreply.github.com>
2018-04-30 16:24:45 +0530

    Update firebase-token-validator.js

--- middleware/firebase-token-validator.js
+++ middleware/firebase-token-validator.js
@@ -1,7 +1,7 @@
 var admin = require('firebase-admin');
 admin.initializeApp({
     credential: admin.credential.cert({
-        "project_id": "ee-lms",
+        "project_id": process.env.FB_ADMIN_PROJECT_ID,
         "private_key": process.env.FB_ADMIN_PRIVATE_KEY.replace(/\\n/g, '\n'),
         "client_email": process.env.FB_ADMIN_CLIENT_EMAIL
     })
