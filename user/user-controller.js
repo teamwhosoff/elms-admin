9a5dedf7ce5316a35cd7a7a8a2d2688df5311ab2
teamwhosoff <38849741+teamwhosoff@users.noreply.github.com>
2018-04-30 17:01:08 +0530

    Update user-controller.js

--- user/user-controller.js
+++ user/user-controller.js
@@ -9,25 +9,28 @@ module.exports.import = (req, res) => {
         store.collection('eUsers').doc(user.email).set(user).then(result => {
             importedList.push(result);
             if (i == arr.length - 1) {
-                res.send(importedList);
+                console.log('basic insert count' + importedList.length);
+                importedList = []
+                users.forEach((user, i, arr) => {
+                if(user.manager) {
+                    user.manager = store.doc("/eUsers/" + user.manager).ref;
+                }
+                if(user.team) {
+                    user.team = store.doc("/eTeam/" + user.team).ref;
+                }
+                store.collection('eUsers').doc(user.email).set(user).then(result => {
+                    importedList.push(result);
+                    if (i == arr.length - 1) {
+                        console.log('reference update count' + importedList.length);
+                        res.send(importedList);
+                    }
+                }).catch(err => res.status(500).send(err))
+            })
             }
         }).catch(err => res.status(500).send(err))
     })
     
-//     users.forEach((user, i, arr) => {
-//         if(user.manager) {
-//             user.manager = store.doc("/eUsers/" + user.manager).ref;
-//         }
-//         if(user.team) {
-//             user.team = store.doc("/eTeam/" + user.team).ref;
-//         }
-//         store.collection('eUsers').doc(user.email).set(user).then(result => {
-//             importedList.push(result);
-//             if (i == arr.length - 1) {
-//                 res.send(importedList);
-//             }
-//         }).catch(err => res.status(500).send(err))
-//     })
+    
 }
 
 module.exports.export = (req, res) => {
