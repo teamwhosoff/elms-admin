083e49d285e9d5247f5657459a23e1b0646fcbea
teamwhosoff <38849741+teamwhosoff@users.noreply.github.com>
2018-04-30 17:17:38 +0530

    Update user-controller.js

--- user/user-controller.js
+++ user/user-controller.js
@@ -7,14 +7,22 @@ module.exports.import = (req, res) => {
 
     users.forEach((user, i, arr) => {
 
-        if (user.manager) {
-            user.manager = store.doc("/eUsers/" + user.manager).ref;
-        }
-        if (user.team) {
-            user.team = store.doc("/eTeam/" + user.team).ref;
-        }
+        // if (user.manager) {
+        //     user.manager = store.doc("/eUsers/" + user.manager).ref;
+        // }
+        // if (user.team) {
+        //     user.team = store.doc("/eTeam/" + user.team).ref;
+        // }
 
         store.collection('eUsers').doc(user.email).set(user).then(result => {
+
+            store.doc('/eUsers/' + user.email).update({ 
+                team: store.doc("/eTeam/" + user.team).ref, 
+                manager: store.doc("/eUsers/" + user.manager).ref 
+            }).then(() => {
+                console.log('updated references for ' + user.email);
+            }).catch(err => { console.log('error while updating references for ' + user.email); console.log(err); })
+
             importedList.push(result);
             if (i == arr.length - 1) {
                 res.send(importedList);
